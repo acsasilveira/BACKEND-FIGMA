e16afbab0783ea333f9b479379abac71278588f9
acsasilveira <acsa.silveiras@gmail.com>
2022-11-14 16:52:42 -0300

    Versao com coisas

--- config/database.ts
+++ config/database.ts
@@ -68,6 +68,9 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
+        ssl: {
+          rejectUnauthorized: false
+        }
       },
       migrations: {
         naturalSort: true,
